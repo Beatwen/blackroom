17dd71a37bf2f269f25712473953ebea914d2a81
Thomas Demolder <thomasdemolder@icloud.com>
2023-11-23 18:08:18 +0100

    Class v1

---
+++ Assets/Player/Iventory.cs
@@ -0,0 +1,7 @@
+using System;
+using Systeme.Collections.Generic;
+
+public class Inventory
+{
+    public List<string> Items { get; set; } = new List<string>();
+}
---
+++ Assets/Player/Spell.cs
@@ -0,0 +1,22 @@
+using System;
+using Systeme.Collections.Generic;
+
+public class Spell
+{
+    // Attrubuts
+    public string Name { get; set; }
+    public int ManaCost { get; set; }
+
+    // Constructeur
+    public Spell(string name, int manaCost)
+    {
+        Name = name;
+        ManaCost = manaCost;
+    }
+
+    // Lancer un sort
+    public void Cast()
+    {
+        Console.WriteLine($"{Name} utilise  (Mana Cost: {ManaCost}).");
+    }
+}
