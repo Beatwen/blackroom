3f31db6ddc1503611839a55c6a9fa407e9ab9720
Beatwen <c_bouserez@hotmail.com>
2023-12-02 18:14:42 +0100

    Player Animation in FightScene

--- Assets/Player/PlayerFigthMode/FigthMode.cs
+++ Assets/Player/PlayerFigthMode/FigthMode.cs
@@ -6,7 +6,9 @@ using UnityEngine.UIElements;
 
 public class FigthMode : Entity
 {
+    [SerializeField] private float movementSpeed = 3.0f;
     private Animator animator;
+    private SpriteRenderer spriteRenderer;
     public float horizontalInput;
     public float verticalInput;
     private float x;
@@ -19,6 +21,7 @@ public class FigthMode : Entity
 
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
         Vector3 startPosition = Vector3.zero;
         x = startPosition.x;
@@ -34,11 +37,23 @@ public class FigthMode : Entity
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 horizontalMovement = Vector3.right.normalized * horizontalInput * Time.deltaTime * 6;
-        Vector3 verticalMovement = Vector3.up.normalized * verticalInput * Time.deltaTime * 6;
+        Vector3 horizontalMovement = Vector3.right.normalized * horizontalInput * Time.deltaTime * 5;
+        Vector3 verticalMovement = Vector3.up.normalized * verticalInput * Time.deltaTime * 5;
         Vector3 totalMovement = horizontalMovement + verticalMovement;
         totalMovement.Normalize();
         transform.Translate(horizontalMovement + verticalMovement);
+        animator.SetFloat("Speed", horizontalInput);
+        animator.SetFloat("SpeedY", verticalInput);
+        if (horizontalInput < 0)
+        {
+            // Face left
+            spriteRenderer.flipX = true;
+        }
+        else if (horizontalInput > 0)
+        {
+            // Face right
+            spriteRenderer.flipX = false;
+        }
 
         if (Input.GetMouseButtonDown(0)) 
         {
