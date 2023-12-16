0afc203553d28194b8e359a18dd287ee0e871ef0
Aubry <arcode.student@gmail.com>
2023-12-16 09:07:59 +0100

    ok

--- Assets/Monsters/Enemy.cs
+++ Assets/Monsters/Enemy.cs
@@ -11,110 +11,33 @@ public class Monsters : Entity
     public bool isDead = false;
     public float moveSpeed = 1f;
     public float rotationSpeed = 1f;
-    float enemySpeedX;
-    float direction;
 
     public float timeBetweenAttacks;
     bool alreadyAttacked;
     private Vector3 previousPosition;
-    public Vector3 walkPoint;
+    public Vector3 walkPoint; // Pour la logique de patrouille future
     private Transform playerPos;
     public float sightRange = 2;
-    public float attackRange= 1;
+    public float attackRange = 1;
     public bool playerIsVisible, playerIsClose;
     public int counter = 0;
 
-    void Patrol()
-    {
-        //Faut que je change ceci, ca peut pas marcher lorsqu'on va rajouter une patrouille
-        animator.SetFloat("Velocity", 0f);
-    }
-    void ChasePlayer()
-    {
-        animator.SetFloat("Velocity", 1f);
-        // Translate towards the player
-        Vector3 direction = (playerPos.position - transform.position).normalized;
-        transform.Translate(direction * moveSpeed * Time.deltaTime, Space.World);
-        CharFlipLogic(direction.x);
-
-        // Look at the player
-        //Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, direction);
-        //transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-    }
-    void AttackPlayer()
-    {
-        animator.SetFloat("Velocity", 0f);
-        if (!alreadyAttacked)
-        {
-            Invoke(nameof(AttackPlayerLogic), 0.2f);
-            animator.SetTrigger("Attack");
-            alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks);
-
-        }
-    }
-    void AttackPlayerLogic()
-    {
-        
-        PlayerFightMode player = playerPos.GetComponent<PlayerFightMode>();
-        if (player != null)
-        {
-            player.TakeDamage(AttackDamage);
-        }
-    }
-    private void ResetAttack()
-    {
-        
-        alreadyAttacked = false;
-    }
     private void Awake()
     {
-        playerPos = GameObject.Find("Player").transform;
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
     }
-
-    public Monsters(string name, int life, int level, int positionX, int positionY)
-    {
-        Name = name;
-        Life = life;
-        Level = level;
-        PositionX = positionX;
-        PositionY = positionY;
-    }
-
-    public void GiveExperience(int experiencePoints)
-    {
-        Debug.Log($"{Name} gives {experiencePoints} experience points.");
-    }
-    
-    public void GiveItem(string item)
-    {
-        Console.WriteLine($"{Name} gives {item}.");
-    }
-    // On Trigger To Hit the Enemy;
-    private void OnTriggerEnter2D(Collider2D collide)
-    {
-        
-        if (collide.CompareTag("Enemy"))
-        {
-            Debug.Log(++counter);
-            PlayerFightMode player = collide.GetComponentInParent<PlayerFightMode>();
-            TakeDamage(player.AttackDamage);
-        }
-    }
-
 
     protected override void Start()
     {
         base.Start();
         previousPosition = transform.position;
     }
+
     protected override void Update()
     {
         base.Update();
 
-        // This is how I check if the player if around
-        Collider2D hit = Physics2D.OverlapCircle(transform.position, sightRange, WhatIsPlayer);
-        playerIsVisible = hit != null;
+        playerIsVisible = Physics2D.OverlapCircle(transform.position, sightRange, WhatIsPlayer);
         playerIsClose = Physics2D.OverlapCircle(transform.position, attackRange, WhatIsPlayer);
 
         if (!playerIsVisible && !playerIsClose)
@@ -130,9 +53,86 @@ public class Monsters : Entity
             AttackPlayer();
         }
     }
+
+    void Patrol()
+    {
+        // Logique de patrouille à implémenter
+        animator.SetFloat("Velocity", 0f);
+    }
+
+    void ChasePlayer()
+    {
+        animator.SetFloat("Velocity", 1f);
+        Vector3 direction = (playerPos.position - transform.position).normalized;
+        transform.Translate(direction * moveSpeed * Time.deltaTime, Space.World);
+        CharFlipLogic(direction.x);
+
+        // Rotation en douceur vers le joueur
+        Quaternion toRotation = Quaternion.LookRotation(playerPos.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    void AttackPlayer()
+    {
+        animator.SetFloat("Velocity", 0f);
+        if (!alreadyAttacked)
+        {
+            // Implémentez la logique d'attaque ici
+            Invoke(nameof(AttackPlayerLogic), 0.2f);
+            animator.SetTrigger("Attack");
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    void AttackPlayerLogic()
+    {
+        PlayerFightMode player = playerPos.GetComponent<PlayerFightMode>();
+        if (player != null)
+        {
+            player.TakeDamage(AttackDamage);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    public Monsters(string name, int life, int level, int positionX, int positionY)
+    {
+        Name = name;
+        Life = life;
+        Level = level;
+        PositionX = positionX;
+        PositionY = positionY;
+    }
+
+    public void GiveExperience(int experiencePoints)
+    {
+        Debug.Log($"{Name} gives {experiencePoints} experience points.");
+    }
+
+    public void GiveItem(string item)
+    {
+        Console.WriteLine($"{Name} gives {item}.");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collide)
+    {
+        if (collide.CompareTag("Enemy"))
+        {
+            Debug.Log(++counter);
+            PlayerFightMode player = collide.GetComponentInParent<PlayerFightMode>();
+            TakeDamage(player.AttackDamage);
+        }
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, sightRange);
     }
+
+    // Ajoutez d'autres méthodes et logiques nécessaires ici
 }
