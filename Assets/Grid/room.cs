446618cf20f6a0f17b71845f4903b31aedba3bde
aetrad <abde@live.be>
2023-12-17 13:55:33 +0100

    Added weapons to the room

--- Assets/Grid/room.cs
+++ Assets/Grid/room.cs
@@ -6,27 +6,61 @@ public class Room : MonoBehaviour
     public string RoomCat { get; set; }
     private bool isVisited;
     private bool isAdjacentToVisited;
-    MainGrid mainGrid = GameManager.instance.mainGrid;
+    MainGrid mainGrid; // Supposition que MainGrid est une classe existante dans votre jeu
     private SpriteRenderer spriteRenderer;
-    public Objet objet { get; set; }
+    public GameObject weaponPrefab; // Préfabriqué pour l'arme
+    private GameObject spawnedWeapon; // Instance de l'arme générée
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        mainGrid = GameManager.instance.mainGrid; // Assurez-vous que cette référence est correcte
     }
+
     private void OnTriggerEnter2D(Collider2D otherCollider)
     {
-        Debug.Log("Trigger entered!");
         if (otherCollider.CompareTag("Player"))
         {
-            Debug.Log("Player entered the room!");
-            
             isVisited = true;
-            
             HighLightedRoom();
-          
+
+            // Générer et afficher l'arme si elle n'existe pas déjà
+            if (spawnedWeapon == null)
+            {
+                SpawnWeapon();
+            }
+            else
+            {
+                ShowWeapon(); // Rendre l'arme visible si elle est déjà générée
+            }
         }
     }
+
+private void SpawnWeapon()
+    {
+        if (weaponPrefab != null)
+        {
+            spawnedWeapon = Instantiate(weaponPrefab, transform.position, Quaternion.identity);
+            spawnedWeapon.SetActive(false); // L'arme est initialement invisible
+            Debug.Log("Arme générée mais invisible");
+        }
+        else
+        {
+            Debug.Log("Préfabriqué d'arme non assigné");
+        }
+    }
+
+
+    // Appelée pour rendre l'arme visible
+public void ShowWeapon()
+{
+    if (spawnedWeapon != null)
+    {
+        spawnedWeapon.SetActive(true);
+        Debug.Log("Arme rendue visible");
+    }
+}
+
 
     public void HighLightedRoom()
     {
@@ -37,8 +71,8 @@ public class Room : MonoBehaviour
             spriteRenderer.color = highlightedColor;
             HighLightAdjacentRoom();
         }
-        
     }
+
     private void HighLightAdjacentRoom()
     {
         (float x, float y)[] relativeCoordinates = {
@@ -60,9 +94,8 @@ public class Room : MonoBehaviour
         }
     }
 
-
     void Update()
     {
-        
+        // Ajoutez ici toute logique de mise à jour supplémentaire si nécessaire
     }
 }
